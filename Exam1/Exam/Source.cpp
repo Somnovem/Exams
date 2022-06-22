bc9fcbce23df00991606bbf790fb3dcad2562988
Somnovem <artem.zhmura606@gmail.com>
2022-06-22 12:51:33 +0200

    Repairing the last commit

--- Exam1/Exam/Source.cpp
+++ Exam1/Exam/Source.cpp
@@ -2,33 +2,105 @@
 #include <string>
 #include <Windows.h>
 #include <fstream>
+std::string lib;
+/// <summary>
+/// Generates a lib of letters
+/// </summary>
+/// <returns>the lib</returns>
+std::string MakeLib() {
+	for (int i = 65; i <= 90; i++)
+	{
+		lib += (char)i;
+	}
+	for (int i = 98; i <= 122; i++)
+	{
+		lib += (char)i;
+	}
+	for (int i = 192; i <= 255; i++)
+	{
+		lib += (char)i;
+	}
+	return lib;
+}
+/// <summary>
+/// Finds the letter in the lib
+/// </summary>
+/// <param name="letter"> - the letter</param>
+/// <returns>pos of the letter</returns>
+int findLetterInLib(char letter) {
+	for (int i = 0; i < lib.length(); i++)
+	{
+		if (lib[i] == letter)
+		{
+			return i;
+		}
+	}
+}
+/// <summary>
+/// Encrypts line with the provided key
+/// </summary>
+/// <param name="str"> - line</param>
+/// <param name="key"> - key</param>
+/// <returns> Encrypted line</returns>
+std::string encrypt(std::string str, std::string key) {
+	int temp;
+	std::string encrypted;
+	for (int i = 0; i < str.length(); i++)
+	{
+				temp = (findLetterInLib(str[i]) + findLetterInLib(key[i])) % lib.length();
+				encrypted += lib[temp];
+	}
+	std::cout << encrypted << std::endl;
+	return encrypted;
+}
+/// <summary>
+/// Decrypts line with the provided key
+/// </summary>
+/// <param name="str"> - line</param>
+/// <param name="key"> - key</param>
+/// <returns>Decrypted line</returns>
+std::string decrypt(std::string str, std::string key) {
+	int temp;
+	std::string encrypted;
+	for (int i = 0; i < str.length(); i++)
+	{
+		temp = findLetterInLib(str[i]) - findLetterInLib(key[i]);
+		if (temp < 0)
+		{
+			temp += lib.length();
+		}
+		encrypted += lib[temp];
+	}
+	std::cout << encrypted << std::endl;
+	return encrypted;
+}
 int main() {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	std::string word, key, cyphered, decyphered;
+	MakeLib();
+	std::string word, key;
 	std::string temp;
 	int choice = 0;
+	std::string filename = "input.txt";
 	while (true)
 	{
-		std::cout << "Выберите хотите ли вы считать строчку или ввести:\n 1 - считать\n 2 - ввести\n" << std::endl;
+		std::cout << "Do you want to read the line or type it in:\n 1 - Read\n 2 - Type In" << std::endl;
 		std::cin >> choice;
 		if (choice == 1)
 		{
-			std::string filename = "input.txt";
 			std::ifstream fileIn;
 			fileIn.open(filename);
 			if (fileIn.fail())
 			{
-				std::cout << "Ошибка открытия файла" << std::endl;
+				std::cout << "Reading failed" << std::endl;
 				return 404;
 			}
 			fileIn >> word;
+			std::cout << "The line: " << word << std::endl;
 			fileIn.close();
 			break;
 		}
 		else if (choice == 2)
 		{
-			std::cout << "Введите слово(английские буквы): ";
+			std::cout << "Type in your word: ";
 			std::cin >> word;
 			std::cout << std::endl;
 			break;
@@ -40,130 +112,56 @@ int main() {
 			std::system("cls");
 		}
 	}
-		std::cout << "Выберите действие:\n 1 - зашифровать\n 2 - расшифровать\n";
-		std::cin >> choice;
-		if (choice == 1)
+	std::cout << "Type in the keyword: " << std::endl;
+	std::cin >> temp;
+	key = word;
+	if (temp.length() > word.length())
+	{
+		for (int i = 0; i < key.length(); i++)
 		{
-			std::cout << "Введите кодовое слово(английские буквы)" << std::endl;
-			std::cin >> temp;
-			key = word;
-			if (temp > word)
-			{
-				int c = 0;
-				for (int i = 0; i < key.size(); i++, c++)
-				{
-					key[i] = temp[c];
-					if (c >= temp.size() - 1) c = -1;
-				}
-			}
-			else if (temp < word) {
-				for (int i = 0; i < key.size(); i++)
-				{
-					key[i] = temp[i];
-				}
-			}
-			else
-			{
-				key = temp;
-			}
-			cyphered = word;
-			std::cout << std::endl;
-			for (int i = 0; i < key.size(); i++) {
-				int lowerBar = (isupper(word[i])) ? 65 : 97;
-				int upperBar = (isupper(key[i])) ? 90 : 122;
-				int k = word[i % word.size()] - lowerBar;
-				int m = 0;
-				if ((isupper(word[i]) && isupper(key[i])) || (!isupper(word[i]) && !isupper(key[i])))
-				{
-					m = key[i];
-				}
-				else
-				{
-					m = key[i] + 32;
-				}
-				if (m + k > upperBar) {
-					cyphered[i] = m + k - 26;
-				}
-				else {
-					cyphered[i] = m + k;
-				}
-			}
-			std::cout << cyphered << std::endl;
-			char answer;
-			std::cout << "Хотите сохранить результат в файл?[y/n] ";
-			std::cin >> answer;
-			if (answer == 'y')
-			{
-				std::string filename = "input.txt";
-				std::ofstream fileOut;
-				fileOut.open(filename);
-				if (fileOut.fail())
-				{
-					std::cout << "Ошибка открытия файла" << std::endl;
-					return 404;
-				}
-				fileOut << cyphered;
-				fileOut.close();
-			}
+			key[i] = temp[i];
 		}
-		else if (choice == 2)
+	}
+	else if (temp.length() < word.length()) {
+		int c = 0;
+		for (int i = 0; i < key.length(); i++, c++)
 		{
-			std::cout << "Введите кодовое слово(английские буквы)" << std::endl;
-			std::cin >> temp;
-			key = word;
-			if (temp < word)
-			{
-				int c = 0;
-				for (int i = 0; i < key.size(); i++, c++)
-				{
-					key[i] = temp[c];
-					if (c >= temp.size() - 1) c = -1;
-				}
-			}
-			else if (temp > word) {
-				for (int i = 0; i < key.size(); i++)
-				{
-					key[i] = temp[i];
-				}
-			}
-			else
-			{
-				key = temp;
-			}
-			decyphered = word;
-			std::cout << std::endl;
-			for (int i = 0; i < decyphered.size(); ++i) {
-				char c = decyphered[i];
-				if (!isalpha(c)) continue;
-				if ((c >= 'A' && c <= 'Z'))
-				{
-					decyphered[i] = (c - toupper(key[i]) + 2 * 'A') % 26 + 'A';
-				}
-				else if (c >= 'a' && c <= 'z') {
-					decyphered[i] = (c - tolower(key[i]) + 2 * 'a') % 26 + 'a';
-				}
-			}
-			std::cout << decyphered << std::endl;
-			char answer = 0;
-			std::cout << "Хотите сохранить результат в файл?[y/n] ";
-			std::cin >> answer;
-			if (answer == 'y')
-			{
-				std::string filename = "input.txt";
-				std::ofstream fileOut;
-				fileOut.open(filename);
-				if (fileOut.fail())
-				{
-					std::cout << "Ошибка открытия файла" << std::endl;
-					return 404;
-				}
-				fileOut << decyphered;
-				fileOut.close();
-			}
+			key[i] = temp[c];
+			if (c >= temp.size() - 1) c = -1;
 		}
-		else
+	}
+	else
+	{
+		key = temp;
+	}
+	std::cout << "What to do with the line:\n 1 - Encrypt\n 2 - Decrypt\n";
+	std::cin >> choice;
+	if (choice == 1)
+	{
+		word = encrypt(word,key);
+	}
+	else if (choice == 2)
+	{
+		word = decrypt(word, key);
+	}
+	else
+	{
+		std::cout << "INVALID INPUT" << std::endl;
+	}
+	char answer;
+	std::cout << "Do you want to save the result to a file?[y/n]: ";
+	std::cin >> answer;
+	if (answer == 'y')
+	{
+		std::ofstream fileOut;
+		fileOut.open(filename);
+		if (fileOut.fail())
 		{
-			std::cout << "Ошибка ввода" << std::endl;
+		    std::cout << "Writing failed" << std::endl;
+			return 404;
 		}
+		fileOut << word;
+		fileOut.close();
+	}
 	return 0;
 }
